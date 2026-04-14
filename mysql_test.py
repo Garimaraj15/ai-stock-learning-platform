941cfa995d30f21993e2d7820c5e87a7bc0874e5
GarimaRaj15 <Garimaraj536@gmail.com>
2026-04-14 19:11:29 +0530

    Initial commit - real-time stock system

---
+++ mysql_test.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Shashi@1415",
+    database="stock_project"
+)
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS stock_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    timestamp DATETIME,
+    price FLOAT,
+    ma5 FLOAT,
+    rsi FLOAT
+)
+""")
+
+print("Table created / already exists")
